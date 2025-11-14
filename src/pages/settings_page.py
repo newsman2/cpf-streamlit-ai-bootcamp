946bc1c0ab0bcb927b45d910f64cda5bfea5f0d5
Edwin <edwin@Edwins-MacBook-Pro.local>
2025-11-14 17:38:11 +0800

    feat: add about, methodology, settings pages

---
+++ src/pages/settings_page.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# region <--------- Streamlit App Configuration --------->
+st.set_page_config(
+    layout="centered",
+    page_title="Settings"
+)
+# endregion <--------- Streamlit App Configuration --------->
+
+st.title("Settings")
