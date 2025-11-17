43d4ef3126cd72bb66f2e37daf6c27c21a3d6d6f
Edwin <edwin@Edwins-MacBook-Pro.local>
2025-11-18 01:28:23 +0800

    docs: updated flow and figure 2

--- src/pages/methodology_page.py
+++ src/pages/methodology_page.py
@@ -15,17 +15,29 @@ st.title("Methodology")
 st.header("CPF Assistant")
 
 st.subheader("Data flow")
-st.markdown(""" This Streamlit-based application enables users' to upload their
-            PDF documents, process them into embeddings using LangChain, store
-            them in a Chroma vector database, and retrieve relevant information
-            through a conversational chat interface powered by an LLM. The
-            Chroma vector database is stored locally in Streamlit community
-            servers. Upon receiving the PDF document, the text will be
-            extracted from the PDF and a single document is created for each
-            PDF. Next, the document will undergo a chunking process using
-            `RecursiveCharacterTextSplitter` with a chunk size of 1000 and
-            chunk overlap of 200. Lastly, the chunked documents will be saved
-            into Chroma vector store and persisted in a file directory. """)
+st.markdown("""
+            There are two data flows, home page and retrieval page in this application.
+
+            **Home Page**
+
+            In the home page, it is a General CPF Assistant that answers questions about
+            Singapore Central Provident Fund. A summary in Chinese will also be
+            displayed for users too.
+
+            **Retrieval Page**
+
+            In the retrieval page, it is a Retrieval Augmented Generation (RAG) page
+            that allows users to upload a PDF document and perform a query on the
+            uploaded documents. The application processes the documents into
+            embeddings using LangChain, store them in a Chroma vector database, and
+            retrieve relevant information through an agent RAG
+            powered by an LLM. The Chroma vector database is stored locally in
+            Streamlit community servers. Upon receiving the PDF document, the
+            text will be extracted from the PDF and a single document is created for
+            each PDF. Next, the document will undergo a chunking process
+            using `RecursiveCharacterTextSplitter` with a chunk size of 1000 and chunk
+            overlap of 200. Lastly, the chunked documents will be saved into
+            Chroma vector store and persisted in a file directory. """)
 
 st.subheader("General CPF Assistant")
 st.image(
@@ -67,20 +79,23 @@ st.image(
 st.markdown(
     """
  Figure 2 shows the data flow for the Retrieval Augmented Generation (RAG) page.
- To use the application RAG, one must upload a PDF document.
+ To use the application RAG, one must upload a PDF document and enter a collection name.
  The application will perform splitting, chunking, and embedding of the PDF document.
  The text chunks will then be stored in a Chroma vector store.
  After which the user can select a collection name and perform a query on the uploaded documents.
  The UI will display an output of the retrieved document chunks with source references.
+
+ If the query matches a tool description of wanting to 'Export to a word document',
+ the tool will be called and a download button will be displayed as the output.
  """
 )
 st.markdown(
     """
     **Test Prompt**
 ```
-Can you help me do the following below. 
+Can you help me do the following below.
 
-Step 1 - Provide a 100-word summary of CareShield 
+Step 1 - Provide a 100-word summary of MediShield
 Step 2 - Export the summary into a single word document
 ```
 """
